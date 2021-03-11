b5b6fa718f5eab30d4020ec5ae826efb6e2d54a5
brionmodernsyntax <brion@modernsyntax.io>
2021-03-10 19:28:39 -0800

    made navigation bar responsive to active page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { ContentComponent } from './content/content.component';
 import { BadgeComponent } from './badge/badge.component';
+import { GamesComponent } from './pages/games/games.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     ContentComponent,
-    BadgeComponent
+    BadgeComponent,
+    GamesComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
